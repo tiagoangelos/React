0c6338e4271ec935138b2df3eef253224dac4ae9
Tiago Angelo <thiagoangelos2020@gmail.com>
2023-05-16 00:19:38 -0300

    criando componetes no react

---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase(){
+    return (
+        <div>
+            <p>Esse é um Componete com uma frase!</p>
+        </div>
+    )
+}
+
+export default Frase
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import Frase from './Frase'
+
+function HelloWorld(){
+    return (
+        <div>
+            <h1>Meu Primeiro Componente!</h1>
+            <Frase />
+        </div>
+    )
+}
+
+export default HelloWorld
