a9917172410d7ffaa3447e916667669c81a3f35c
Tiago Angelo <thiagoangelos2020@gmail.com>
2023-06-04 22:21:18 -0300

    react icons em react js

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,6 +1,16 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import style from './Footer.module.css';
+
 function Footer(){
     return (
-        <footer>@all copyright reserved</footer>
+        <footer>
+            <ul className={style.social_list}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin /></li>
+            </ul>
+            <p>@Todos Direitos Reservados</p>
+        </footer>
     )
 }
 
